79435ecd2748fe787a516bfd1a6bfd2412dd623e
victoriacheungwustl <71216093+victoriacheungwustl@users.noreply.github.com>
2023-10-20 12:37:20 -0500

    updated home and subdomain page

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -31,6 +31,120 @@ export default function Home() {
           <p>Contact a Specialist:</p>
         </div>
       </div>
+      <div className="border bg-white flex w-[707px] flex-col border-solid border-black">
+      {/* <div className="bg-zinc-300 self-stretch flex w-full items-start justify-between gap-5 mt-1 pl-3.5 pr-5 py-1.5 max-md:flex-wrap max-md:justify-center">
+        <div className="text-black text-xs font-medium bg-white w-[49px] max-w-full -mt-px pl-1.5 pr-5 pt-3 pb-1.5">
+          logo
+          <br />
+        </div>
+        <div className="flex items-start gap-3 -mt-0.5 max-md:justify-center">
+          <div className="text-black text-xs font-medium self-stretch bg-white w-[49px] max-w-full pr-3 pt-1.5 pb-2.5">
+            Physical
+          </div>
+          <div className="text-black text-xs font-medium self-stretch bg-white w-[49px] max-w-full pr-5 pt-2 pb-2.5">
+            Mental
+          </div>
+          <div className="bg-white self-stretch flex w-[49px] h-[25px] flex-col" />
+          <div className="bg-white self-stretch flex w-[49px] h-[25px] flex-col" />
+          <div className="bg-white self-stretch flex w-[49px] h-[25px] flex-col" />
+        </div>
+        <div className="flex items-start gap-3.5 -mt-0.5">
+          <div className="text-black text-xs font-medium self-stretch bg-white w-[49px] max-w-full pl-3.5 pr-3 pt-2 pb-2">
+            log in
+          </div>
+          <div className="text-black text-xs font-medium self-stretch bg-white w-[49px] max-w-full pl-2 pr-3 py-2.5">
+            search
+          </div>
+        </div>
+      </div> */}
+      <div className="bg-zinc-100 self-stretch flex w-full h-[183px] flex-col max-md:max-w-full" />
+      <div className="bg-zinc-300 self-center flex w-[377px] h-[34px] flex-col ml-3 mt-9" />
+      <div className="self-center w-[495px] max-w-full mt-11 px-5 max-md:mt-10">
+        <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
+          <div className="flex flex-col items-stretch w-[34%] max-md:w-full max-md:ml-0">
+            <div className="flex grow flex-col w-[136px] max-md:mt-9">
+              <div className="bg-zinc-300 self-stretch flex w-full h-[172px] flex-col max-md:mr-px" />
+              <div className="bg-zinc-300 self-stretch flex w-full h-6 flex-col mt-6 max-md:mr-px" />
+              <div className="bg-zinc-300 self-stretch flex w-full h-[172px] flex-col mt-11 max-md:mt-10" />
+              <div className="bg-zinc-300 self-stretch flex w-full h-6 flex-col mt-6" />
+            </div>
+          </div>
+          <div className="flex flex-col items-stretch w-[34%] ml-5 max-md:w-full max-md:ml-0">
+            <div className="flex grow flex-col max-md:mt-9">
+              <div className="bg-zinc-300 flex w-[135px] h-[172px] flex-col" />
+              <div className="bg-zinc-300 flex w-[135px] h-6 flex-col mt-6" />
+              <div className="bg-zinc-300 flex w-[135px] h-[172px] flex-col mt-11 max-md:ml-2.5 max-md:mt-10" />
+              <div className="bg-zinc-300 flex w-[135px] h-6 flex-col mt-6 max-md:ml-2.5" />
+            </div>
+          </div>
+          <div className="flex flex-col items-stretch w-[32%] ml-5 max-md:w-full max-md:ml-0">
+            <div className="flex grow flex-col w-[135px] max-md:mt-9">
+              <div className="bg-zinc-300 self-stretch flex w-full h-[172px] flex-col" />
+              <div className="bg-zinc-300 self-stretch flex w-full h-6 flex-col mt-6" />
+              <div className="bg-zinc-300 self-stretch flex w-full h-[172px] flex-col mt-11 max-md:mt-10" />
+              <div className="bg-zinc-300 self-stretch flex w-full h-6 flex-col mt-6" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-zinc-300 self-stretch flex flex-col mt-11 px-5 py-14 max-md:max-w-full max-md:mt-10">
+        <div className="self-center flex ml-0 w-[386px] max-w-full flex-col -mb-px">
+          <div className="bg-white flex w-full h-10 flex-col" />
+          <div className="bg-white flex w-52 h-10 flex-col mt-14 max-md:mt-10" />
+          <div className="bg-white flex w-[330px] h-[162px] flex-col mt-11 max-md:mt-10" />
+        </div>
+      </div>
+      <div className="self-center w-[637px] max-w-full mt-20 px-5 max-md:mt-10">
+        <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
+          <div className="flex flex-col items-stretch w-[6%] max-md:w-full max-md:ml-0">
+            <img
+              loading="lazy"
+              srcSet="..."
+              className="aspect-[0.89] object-cover object-center w-[34px] rotate-[-88.709deg] fill-zinc-300 overflow-hidden max-w-full shrink-0 my-auto max-md:mt-10"
+            />
+          </div>
+          <div className="flex flex-col items-stretch w-[88%] ml-5 max-md:w-full max-md:ml-0">
+            <div className="bg-zinc-300 flex w-[491px] h-[249px] flex-col mx-auto max-md:max-w-full max-md:mt-10" />
+          </div>
+          <div className="flex flex-col items-stretch w-[6%] ml-5 max-md:w-full max-md:ml-0">
+            <img
+              loading="lazy"
+              srcSet="..."
+              className="aspect-[0.87] object-cover object-center w-[33px] rotate-90 fill-zinc-300 overflow-hidden max-w-full shrink-0 my-auto max-md:mt-10"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="bg-zinc-300 self-center flex w-[491px] h-[39px] flex-col mt-11 max-md:max-w-full max-md:mt-10" />
+      <div className="bg-zinc-300 self-stretch flex grow flex-col mt-20 pt-7 px-5 max-md:max-w-full max-md:mt-10">
+        <div className="self-center w-[622px] max-w-full">
+          <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
+            <div className="flex flex-col items-stretch w-[33%] max-md:w-full max-md:ml-0">
+              <div className="flex flex-col w-[188px] max-md:mt-7">
+                <div className="bg-white self-stretch flex w-full h-[33px] flex-col" />
+                <div className="bg-white self-stretch flex w-full h-[33px] flex-col mt-3.5" />
+                <div className="bg-white self-stretch flex w-full h-[33px] flex-col mt-3.5" />
+              </div>
+            </div>
+            <div className="flex flex-col items-stretch w-[33%] ml-5 max-md:w-full max-md:ml-0">
+              <div className="flex grow flex-col w-[188px] max-md:mt-7">
+                <div className="bg-white self-stretch flex w-full h-[33px] flex-col" />
+                <div className="bg-white self-stretch flex w-full h-[33px] flex-col mt-3.5" />
+                <div className="bg-white self-stretch flex w-full h-[33px] flex-col mt-3.5" />
+                <div className="bg-white self-center flex w-[75px] h-[35px] flex-col mt-3.5" />
+              </div>
+            </div>
+            <div className="flex flex-col items-stretch w-[33%] ml-5 max-md:w-full max-md:ml-0">
+              <div className="flex flex-col w-[188px] max-md:mt-7">
+                <div className="bg-white self-stretch flex w-full h-[33px] flex-col" />
+                <div className="bg-white self-stretch flex w-full h-[33px] flex-col mt-3.5" />
+                <div className="bg-white self-stretch flex w-full h-[33px] flex-col mt-3.5" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     </main>
   )
 }
--- my-app/app/physical/page.jsx
+++ my-app/app/physical/page.jsx
@@ -1,8 +1,191 @@
-import React from 'react'
+import React from 'react';
 
 const Page = () => {
   return (
-    <div className='p-4'>Physical Subdomain placeholder</div>
+    <><div className="border bg-white flex w-[707px] flex-col border-solid border-black">
+      
+      <div className="bg-zinc-100 self-stretch flex flex-col pt-16 pb-24 px-5 max-md:max-w-full">
+        <div className="self-center flex w-[177px] max-w-full flex-col">
+          <div className="text-black text-center text-2xl font-medium max-md:ml-2.5">
+            Headline Topic
+          </div>
+          <div className="text-black text-center text-lg font-medium mt-8">
+            Headline Description
+          </div>
+        </div>
+      </div>
+      <div className="self-center w-[567px] max-w-full -ml-3 mt-20 px-5 max-md:mt-10">
+        <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
+          <div className="flex flex-col items-stretch w-[36%] max-md:w-full max-md:ml-0">
+            <div className="bg-zinc-300 flex w-[185px] h-[228px] flex-col mx-auto max-md:mt-10" />
+          </div>
+          <div className="flex flex-col items-stretch w-[64%] ml-5 max-md:w-full max-md:ml-0">
+            <div className="flex flex-col my-auto max-md:mt-10">
+              <div className="text-black text-sm font-medium">
+                Sub-Headline 1
+              </div>
+              <div className="text-black text-xs mt-10">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Diam
+                vel quam elementum pulvinar etiam non quam lacus. Elit sed
+                vulputate mi sit amet mauris commodo quis imperdiet. Diam
+                maecenas sed enim ut sem viverra aliquet eget sit. Nec feugiat
+                nisl pretium fusce id. Ridiculus mus mauris vitae ultricies leo
+                integer malesuada nunc vel. Interdum velit laoreet id donec
+                ultrices tincidunt arcu.<br />
+                <br />
+                Massa massa ultricies mi quis hendrerit dolor magna eget est.
+                Sed nisi lacus sed viverra tellus in hac habitasse. Magna eget
+                est lorem ipsum. Egestas purus viverra accumsan in nisl nisi
+                scelerisque. At auctor urna nunc id cursus metus aliquam. Turpis
+                egestas maecenas pharetra convallis posuere morbi leo urna
+                molestie. Eget arcu dictum varius duis at consectetur lorem
+                donec massa.{" "}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-white flex w-[29px] h-[59px] flex-col" />
+      <div className="self-center w-[495px] max-w-full mt-6 px-5">
+        <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
+          <div className="flex flex-col items-stretch w-[33%] max-md:w-full max-md:ml-0">
+            <div className="flex grow flex-col w-[135px] max-md:mt-10">
+              <div className="bg-zinc-300 self-stretch flex w-full h-[172px] flex-col" />
+              <div className="bg-zinc-300 self-stretch flex w-full h-6 flex-col mt-6" />
+            </div>
+          </div>
+          <div className="flex flex-col items-stretch w-[33%] ml-5 max-md:w-full max-md:ml-0">
+            <div className="flex grow flex-col w-[135px] max-md:mt-10">
+              <div className="bg-zinc-300 self-stretch flex w-full h-[172px] flex-col" />
+              <div className="bg-zinc-300 self-stretch flex w-full h-6 flex-col mt-6" />
+            </div>
+          </div>
+          <div className="flex flex-col items-stretch w-[33%] ml-5 max-md:w-full max-md:ml-0">
+            <div className="flex grow flex-col w-[135px] max-md:mt-10">
+              <div className="bg-zinc-300 self-stretch flex w-full h-[172px] flex-col" />
+              <div className="bg-zinc-300 self-stretch flex w-full h-6 flex-col mt-6" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-zinc-300 self-stretch flex flex-col mt-12 px-5 py-14 max-md:max-w-full max-md:mt-10">
+        <div className="flex w-[638px] max-w-full flex-col -mt-px max-md:mr-1.5">
+          <div className="self-stretch max-md:max-w-full">
+            <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
+              <div className="flex flex-col items-stretch w-[6%] max-md:w-full max-md:ml-0">
+                <img
+                  loading="lazy"
+                  srcSet="..."
+                  className="aspect-[0.92] object-cover object-center w-[35px] rotate-[-88.709deg] fill-white overflow-hidden max-w-full shrink-0 my-auto max-md:mt-10" />
+              </div>
+              <div className="flex flex-col items-stretch w-[88%] ml-5 max-md:w-full max-md:ml-0">
+                <div className="bg-white flex w-[491px] h-[249px] flex-col mx-auto max-md:max-w-full max-md:mt-10" />
+              </div>
+              <div className="flex flex-col items-stretch w-[6%] ml-5 max-md:w-full max-md:ml-0">
+                <img
+                  loading="lazy"
+                  srcSet="..."
+                  className="aspect-[0.89] object-cover object-center w-[34px] rotate-90 fill-white overflow-hidden max-w-full shrink-0 my-auto max-md:mt-10" />
+              </div>
+            </div>
+          </div>
+          <div className="bg-white self-center flex w-[491px] h-[39px] flex-col mt-11 max-md:max-w-full max-md:mt-10" />
+        </div>
+      </div>
+      <div className="self-center w-[409px] max-w-full mt-20 px-5 max-md:mt-10">
+        <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
+          <div className="flex flex-col items-stretch w-[33%] max-md:w-full max-md:ml-0">
+            <div className="flex grow flex-col max-md:mt-10">
+              <div className="text-black text-xl">Document </div>
+              <div className="text-black text-xs mt-3">
+                Lorem ipsum dolor sit amet, consectetur
+              </div>
+              <div className="text-black text-xl mt-14 max-md:mt-10">
+                Document{" "}
+              </div>
+              <div className="text-black text-xs mt-3">
+                Lorem ipsum dolor sit amet, consectetur
+              </div>
+              <div className="text-black text-xl mt-14 max-md:mt-10">
+                Document{" "}
+              </div>
+              <div className="text-black text-xs mt-3">
+                Lorem ipsum dolor sit amet, consectetur
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col items-stretch w-[33%] ml-5 max-md:w-full max-md:ml-0">
+            <div className="flex grow flex-col max-md:mt-10">
+              <div className="text-black text-xl">Document </div>
+              <div className="text-black text-xs mt-3">
+                Lorem ipsum dolor sit amet, consectetur
+              </div>
+              <div className="text-black text-xl mt-14 max-md:mt-10">
+                Document{" "}
+              </div>
+              <div className="text-black text-xs mt-3">
+                Lorem ipsum dolor sit amet, consectetur
+              </div>
+              <div className="text-black text-xl mt-14 max-md:mt-10">
+                Document{" "}
+              </div>
+              <div className="text-black text-xs mt-3">
+                Lorem ipsum dolor sit amet, consectetur
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col items-stretch w-[33%] ml-5 max-md:w-full max-md:ml-0">
+            <div className="flex grow flex-col max-md:mt-10">
+              <div className="text-black text-xl">Document </div>
+              <div className="text-black text-xs mt-3">
+                Lorem ipsum dolor sit amet, consectetur
+              </div>
+              <div className="text-black text-xl mt-14 max-md:mt-10">
+                Document{" "}
+              </div>
+              <div className="text-black text-xs mt-3">
+                Lorem ipsum dolor sit amet, consectetur
+              </div>
+              <div className="text-black text-xl mt-14 max-md:mt-10">
+                Document{" "}
+              </div>
+              <div className="text-black text-xs mt-3">
+                Lorem ipsum dolor sit amet, consectetur
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-zinc-300 self-stretch flex flex-col mt-16 mb-9 pt-7 pb-2 px-5 max-md:max-w-full max-md:mt-10">
+        <div className="self-center w-[622px] max-w-full">
+          <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
+            <div className="flex flex-col items-stretch w-[33%] max-md:w-full max-md:ml-0">
+              <div className="flex flex-col w-[188px] max-md:mt-7">
+                <div className="bg-white self-stretch flex w-full h-[33px] flex-col" />
+                <div className="bg-white self-stretch flex w-full h-[33px] flex-col mt-3.5" />
+                <div className="bg-white self-stretch flex w-full h-[33px] flex-col mt-3.5" />
+              </div>
+            </div>
+            <div className="flex flex-col items-stretch w-[33%] ml-5 max-md:w-full max-md:ml-0">
+              <div className="flex grow flex-col w-[188px] max-md:mt-7">
+                <div className="bg-white self-stretch flex w-full h-[33px] flex-col" />
+                <div className="bg-white self-stretch flex w-full h-[33px] flex-col mt-3.5" />
+                <div className="bg-white self-stretch flex w-full h-[33px] flex-col mt-3.5" />
+                <div className="bg-white self-center flex w-[75px] h-[72px] flex-col mt-3.5" />
+              </div>
+            </div>
+            <div className="flex flex-col items-stretch w-[33%] ml-5 max-md:w-full max-md:ml-0">
+              <div className="flex flex-col w-[188px] max-md:mt-7">
+                <div className="bg-white self-stretch flex w-full h-[33px] flex-col" />
+                <div className="bg-white self-stretch flex w-full h-[33px] flex-col mt-3.5" />
+                <div className="bg-white self-stretch flex w-full h-[33px] flex-col mt-3.5" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div></>
   )
 }
 
