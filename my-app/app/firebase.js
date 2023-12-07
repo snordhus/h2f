ce8e6db192412e07790d003cd847bb9bd656e1c2
Samuel Nordhus <70170846+snordhus@users.noreply.github.com>
2023-12-07 14:16:26 -0600

    reset

--- my-app/app/firebase.js
+++ my-app/app/firebase.js
@@ -8,18 +8,27 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// ORIGINAL FIREBASE
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDo0b5DNRcNP3BDYZfs5yD358CVx4NnOnU",
+//   authDomain: "h2fsite-fda7d.firebaseapp.com",
+//   projectId: "h2fsite-fda7d",
+//   storageBucket: "h2fsite-fda7d.appspot.com",
+//   messagingSenderId: "170553094364",
+//   appId: "1:170553094364:web:e23bfc772555aa63008419",
+//   measurementId: "G-HX16XH6WVQ"
+// };
+
+// ALTERNATIVE FIREBASE
 const firebaseConfig = {
-  apiKey: "AIzaSyDo0b5DNRcNP3BDYZfs5yD358CVx4NnOnU",
-  authDomain: "h2fsite-fda7d.firebaseapp.com",
-  projectId: "h2fsite-fda7d",
-  storageBucket: "h2fsite-fda7d.appspot.com",
-  messagingSenderId: "170553094364",
-  appId: "1:170553094364:web:e23bfc772555aa63008419",
-  measurementId: "G-HX16XH6WVQ"
+  apiKey: "AIzaSyAHwLrfWnHqe1cBfMwwyrdrV6A9Ti4VQaQ",
+  authDomain: "h2fsite2.firebaseapp.com",
+  projectId: "h2fsite2",
+  storageBucket: "h2fsite2.appspot.com",
+  messagingSenderId: "646360996992",
+  appId: "1:646360996992:web:b1d74f19176c141a67da65"
 };
 
-// Initialize Firebase
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
---
+++ my-app/app/sleep copy/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Documents from './../Documents';
+
+const Page = () => {
+  return (
+    <main className="p-4">
+      <h1 class="text-4xl pb-8">All H2F Documents:</h1>
+      <Documents />
+    </main>
+  )
+}
+
+export default Page
